ee5d526dd47af4c08f2100d3373aef726768e3da
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-09-17 19:43:30 +0200

    Oppdateringer for å få inn basic fra designsystemet

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import "./_app.less";
+import "nav-frontend-typografi-style/src/index.less";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="app">
+      <Component {...pageProps} />
+
+      <style jsx>{`
+        div {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          max-width: 100%;
+          min-height: 20rem;
+          margin: auto;
+          padding: 0.5rem 0.5rem 7.5rem 0.5rem;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
 }
-
-export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,88 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import HomeIkon from "../public/assets/svg/home.svg";
+import Lenkepanel from "nav-frontend-lenkepanel";
+import { Knapp } from "nav-frontend-knapper";
+import {
+  Normaltekst,
+  Sidetittel,
+  Ingress,
+  Undertittel,
+} from "nav-frontend-typografi";
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <div className={styles.container}>
+    <div className="root">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Dagpengre!</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <section>
+        <div className="fo">
+          <HomeIkon style={{ height: "20px" }} />
+          <Sidetittel>Hola!</Sidetittel>
+          <Ingress>Nå tester vi quiz</Ingress>
         </div>
+      </section>
+
+      <main>
+        <div className="boxes">
+          <Link href="/quiz">
+            <Lenkepanel className="half" href="/quiz" border>
+              <Undertittel>Ta en quiz om dagpenger</Undertittel>
+              <Normaltekst>Kul link som loader instantly</Normaltekst>
+            </Lenkepanel>
+          </Link>
+
+          <Link href="/quiz">
+            <Lenkepanel className="half" href="/quiz" border>
+              <Undertittel>Finn ut hva du har krav på</Undertittel>
+              <Normaltekst>Kul link som gir mad speeds</Normaltekst>
+            </Lenkepanel>
+          </Link>
+        </div>
+        <Normaltekst>PartyTime</Normaltekst>
+        <Link href="/quiz">
+          <Knapp>
+            <a>Ta en quiz veldig fort!</a>
+          </Knapp>
+        </Link>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <style jsx>{`
+        .root {
+          max-width: 100%;
+          display: flex;
+          flex-direction: column;
+          margin: auto;
+          width: 1000px;
+          margin: 0 0.5rem 0 0.5rem;
+        }
+        
+        .boxes {
+          display: flex;
+          flex-direction: row;
+          flex-wrap: row;
+          width: 100%;
+        }
+
+        .root :global(.half:not(:nth-child(2n))) {
+          margin-right: 1rem;
+        }
+        .root :global(.half) {
+          display: flex;
+          flex-direction: row;
+          padding: 1rem;
+          flex-grow: 1;
+          flex-shrink: 0;
+          max-width: calc(50% - 0.5rem);
+        }
+
+        section {
+          justify-content: center;
+          text-align: center;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
