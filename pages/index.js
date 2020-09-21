bb97fe2ff3b0b0e22288d5370b797f39263ac3f4
Johan Rusvik <johan.rusvik@nav.no>
2020-09-21 14:04:17 +0200

    Korrekt url

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home (props) {
   const [status, setStatus] = useState('IKKE_SENDT')
   const handleOverforing = async () => {
     const id = props.registrering.id
-    const { data } = await axios(`http://localhost:3000/api/put-registrering?id=${id}`)
+    const { data } = await axios(`http://localhost:3000/arbeid/arbeidssokerregistrering-veileder/api/put-registrering?id=${id}`)
     const { status } = data
     setStatus(status)
   }
