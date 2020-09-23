f1ab9ae4f534319b40a3137b4323947ac1af3c08
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-09-23 07:24:26 +0200

    Bryter ut overføring til Arena som egen component

---
+++ components/overfor-til-arena.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { AlertStripeSuksess, AlertStripeFeil } from 'nav-frontend-alertstriper'
+import { Hovedknapp } from 'nav-frontend-knapper'
+
+const OverforTilArena  = props => {
+  const { id } = props
+  const [status, setStatus] = useState('IKKE_SENDT')
+
+  const handleOverforing = async () => {
+    const { data } = await axios(`${process.env.NEXT_PUBLIC_API_URL}/put-registrering?id=${id}`)
+    const { status } = data
+    setStatus(status)
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    handleOverforing()
+  }
+  return (
+    <>
+      {status === 'IKKE_SENDT' && <Hovedknapp onClick={handleClick}>Overfør til Arena</Hovedknapp>}
+      {status === 'FEIL' && <AlertStripeFeil>Noe gikk galt under overføringen</AlertStripeFeil>}
+      {status === 'SENDT' && <AlertStripeSuksess>Brukeren er overført</AlertStripeSuksess>}
+    </>
+  )
+}
+
+export default OverforTilArena
