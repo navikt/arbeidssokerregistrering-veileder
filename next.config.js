39218a36961ffb45e690b111542f4a3deffb5b55
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-09-28 15:06:29 +0200

    Legger til next-auth

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ module.exports = {
         destination: '/api/alive-and-ready'
       },
       {
+        source: '/oauth2/callback',
+        destination: '/api/oauth2/callback'
+      },
+      {
         source: '/isReady',
         destination: '/api/alive-and-ready'
       }
